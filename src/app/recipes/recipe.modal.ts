5a31063eb33a9f04b0e1b24b8494c604551b3cb4
rajasekar412 <38438735+rajasekar412@users.noreply.github.com>
2021-06-07 16:40:38 -0500

    shopping cart

---
+++ src/app/recipes/recipe.modal.ts
@@ -0,0 +1,13 @@
+export class Recipe{
+    public name: string;
+    public description: string;
+    public imagePath: string;
+    
+    constructor(name: string, descrption: string, imagePath: string){
+        this.name = name;
+        this.description = descrption;
+        this.imagePath = imagePath;
+    
+    }
+    
+    }
